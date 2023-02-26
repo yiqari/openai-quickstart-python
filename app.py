a84ca87c85b4cd521de90eff725a56ee08cbe78e
yiqari <125476298+yiqari@users.noreply.github.com>
2023-02-25 16:18:13 -0800

    OpenAI Flask server

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import os
 
 import openai
+import socket
 from flask import Flask, redirect, render_template, request, url_for
 
 app = Flask(__name__)
@@ -10,26 +11,46 @@ openai.api_key = os.getenv("OPENAI_API_KEY")
 @app.route("/", methods=("GET", "POST"))
 def index():
     if request.method == "POST":
-        animal = request.form["animal"]
+        print("************TEST********")
+        print("************TEST********")
+        print("************TEST********")
+        print("************TEST********")
+        print("************TEST********")
+        print("************TEST********")
+
+        input = request.json["input"]
+        print(input)
+        print("************TEST********")
+        hostname = socket.gethostname()
+        PAddr = request.remote_addr
+        print("hostname: " + hostname + " IP Address: " + PAddr)
+        print(request.environ.get('HTTP_X_REAL_IP', request.remote_addr))
         response = openai.Completion.create(
             model="text-davinci-003",
-            prompt=generate_prompt(animal),
+            max_tokens = 256,
+            stop=None,
+            prompt=input,
             temperature=0.6,
         )
+        print(response)
+
         return redirect(url_for("index", result=response.choices[0].text))
 
     result = request.args.get("result")
-    return render_template("index.html", result=result)
+
+    return result
+
+    # return render_template("index.html", result="test123")
 
 
-def generate_prompt(animal):
+def generate_prompt(input):
     return """Suggest three names for an animal that is a superhero.
 
-Animal: Cat
+input: Cat
 Names: Captain Sharpclaw, Agent Fluffball, The Incredible Feline
-Animal: Dog
+input: Dog
 Names: Ruff the Protector, Wonder Canine, Sir Barks-a-Lot
-Animal: {}
+input: {}
 Names:""".format(
-        animal.capitalize()
+        input.capitalize()
     )
